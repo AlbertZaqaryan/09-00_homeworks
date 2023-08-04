b94d587bf709ec75a18c75710345c00917050cde
AlbertZaqaryan <itm.albert.zaqaryan@gmail.com>
2023-08-04 09:59:08 +0400

    set

---
+++ new.py
@@ -0,0 +1,26 @@
+# set1 = set()
+# print(type(set1))
+
+# set1 = {2, 2, 4, 5, 6, 1}
+# print(set1)
+
+# list1 = [4, 4, 4, 4, 4, 5, 6, 9, 8, 7, 4, 1]
+# print(list(set(list1)))
+
+# my_set1 = {1, 2, 3}
+# # my_set1.add(4)
+# my_set1.discard(2)
+# print(my_set1)
+
+
+# my_set1 = {'a', 'b', 'c'}
+# print(my_set1)
+
+# set1 = {1, 2, 3, 4, 5}
+# set2 = {2, 3}
+# print(set2.issubset(set1))
+# print(set1.issuperset(set2))
+
+# set1 = {1, 2, 3}
+# set2 = {4, 5, 6, 9}
+# print(set1.isdisjoint(set2))
